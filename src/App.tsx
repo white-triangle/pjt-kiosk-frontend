624ac0d914c523123ded8c09a615235c671c11d5
yang <ysotgood@gmail.com>
2025-01-26 16:03:26 +0900

    init: prettierrc add

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 function App() {
 // 초기세팅 2025-01-19
   return (
-    <>
-      <div className="card">
-        <p>
-          <h1 className="text-md font-bold underline ">
-            Hello world!
-          </h1>
-        </p>
-      </div>
-
-    </>
+      <>
+          <div className='card'>
+              <p>
+                  <h1 className='text-md font-bold underline '>Hello world!</h1>
+              </p>
+          </div>
+      </>
   )
 }
 
