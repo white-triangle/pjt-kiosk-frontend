044c2339e3e47bccee8d75eeadbbbeb5e335b60f
yang <ysotgood@gmail.com>
2025-02-25 19:21:57 +0900

    feature: App.tsx 브라우저 라우팅 구현(어드민)

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,18 +1,15 @@
+import { Outlet } from 'react-router-dom'
 import NavigationBar from '@/shared/admin/ui/NavigationBar'
 import SideNavigationBar from '@/shared/admin/ui/SideNavigationBar'
-import AdminDashboard from './dashboard'
-import Menu from './menu'
 import './AdminMain.scss'
-import Table from './table'
+
 export default function AdminMain() {
     return (
         <div className='admin-main'>
             <NavigationBar />
             <SideNavigationBar />
             <div className='content-wrapper'>
-                {/* <AdminDashboard /> */}
-                {/* <Menu /> */}
-                <Table />
+                <Outlet />
             </div>
         </div>
     )
